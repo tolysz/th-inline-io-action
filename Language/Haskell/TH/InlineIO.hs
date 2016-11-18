795dad439f35ae2f1f3c13e09ddd4d756a201fb3
Marcin Tolysz <tolysz@gmail.com>
2016-11-18 16:47:56 +0000

    add some documentation

--- Language/Haskell/TH/InlineIO.hs
+++ Language/Haskell/TH/InlineIO.hs
@@ -6,6 +6,8 @@ where
 
 import qualified Language.Haskell.TH.Syntax as TH
 
+-- | Execute some IO action during compilation only then return the value,
+-- from the application perspective it will be a static value
 inlineIOAction :: TH.Lift a => (IO a) -> TH.Q (TH.TExp a)
 inlineIOAction act = do
   a <- TH.runIO act
